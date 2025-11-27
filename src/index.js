1d3de2ffcd911ca1739648aa98c50f2479ddb293
JoseFaccirolli <jose.faccirolli08@gmail.com>
2025-11-27 01:44:14 -0300

    Comentários de aprendizagem no index

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ class AppController {
 }
 
 module.exports = new AppController().express;
+
+/*
+
+    Aprendizados:
+- Por que usar classes?
+As classes servem para modularizar e organizar o código, isso facilita o crescimento
+do projeto, para qunado ele ficar grande, esteja organizado e funcional.
+
+- O que é uma instância?
+Instancia é o objeto da classe, ou seja, a classe serve como 
+um molde, suas instancias são o uso desse molde aplicado.
+
+- O que são middlewares?
+Os middlewares são intermediadores, eles processam a requisição 
+ANTES de chegar nas rotas, fazendo uma função / tarefa repetitiva.
+
+*/
