b5da36c605e565534e0c64141634fdae83b26346
JoseFaccirolli <jose.faccirolli08@gmail.com>
2025-11-27 01:43:53 -0300

    Adicao da estrutura inicial do index

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+class AppController {
+    constructor() {
+        this.express = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.express.use(express.json());
+    }
+
+    routes(){
+        const apiRoutes = require('./routes/apiRoutes');
+        this.express.use('/estoque', apiRoutes);
+    }
+}
+
+module.exports = new AppController().express;
