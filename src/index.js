30d03b063cfced74573999f2daf036d9f13e05ae
JoseFaccirolli <jose.faccirolli08@gmail.com>
2026-03-01 22:18:59 -0300

    Add: Configuracoes de cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const testConnect = require("./database/testConnect");
+const cors = require("cors");
 
 class AppController {
     constructor() {
@@ -11,6 +12,10 @@ class AppController {
 
     middlewares() {
         this.express.use(express.json());
+        this.express.use(cors({
+            origin: process.env.FRONT_URL || "http://localhost:5173",
+            methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        }));
     }
 
     routes(){
